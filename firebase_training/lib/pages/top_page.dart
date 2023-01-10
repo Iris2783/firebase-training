ffc4cfb0156482c7bf253caaae3086b975ccf425
Iris2783 <iris.corp.2783@gmail.com>
2023-01-10 17:58:28 +0900

    update

--- firebase_training/lib/pages/top_page.dart
+++ firebase_training/lib/pages/top_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_training/model/memo.dart';
+import 'package:firebase_training/pages/memo_detail_page.dart';
 import 'package:flutter/material.dart';
 
 class TopPage extends StatefulWidget {
@@ -17,7 +18,9 @@ class _TopPageState extends State<TopPage> {
 
 //以下のコードを記述することで実際にFirebaseからデータを取得する
   Future<void> fetchMemo() async {
-    final memoCollection = await FirebaseFirestore.instance.collection('memo').get(); //FirebaseのMemoコレクションにアクセスするためのコード
+    final memoCollection = await FirebaseFirestore.instance
+        .collection('memo')
+        .get(); //FirebaseのMemoコレクションにアクセスするためのコード
     final docs = memoCollection.docs; //Memoコレクションのドキュメントにアクセスするためのコード
     //以下のコードで登録されているMemoコレクションのドキュメントの数だけ処理(データを取り出す)を行うようにする
     for (var doc in docs) {
@@ -47,14 +50,25 @@ class _TopPageState extends State<TopPage> {
         title: const Text('Fluter × Firebase'),
       ),
       body: ListView.builder(
-          itemCount:
-              memoList.length, //memoListの中にある要素の数だけ表示させたい場合は関数名+lengthを記述する
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(
-                  memoList[index].title), //indexを使うことで順番の指定なくそのまま順番に表示してくれる
-            );
-          }),
+        itemCount:
+            memoList.length, //memoListの中にある要素の数だけ表示させたい場合は関数名+lengthを記述する
+        itemBuilder: (context, index) {
+          return ListTile(
+            title:
+                Text(memoList[index].title), //indexを使うことで順番の指定なくそのまま順番に表示してくれる
+            //確認画面に遷移する記述を以下に書く
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MemoDetailPage(memoList[
+                      index]), //memoList[index]を挿入することでタップされたメモの情報を詳細ページに送ることができる
+                ),
+              );
+            },
+          );
+        },
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         tooltip: 'Increment',
